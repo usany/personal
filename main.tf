cbf56ffb5a2382b731aa0db1de1f390cbf482538
charlie <ckd_qja@naver.com>
2026-09-06 17:17:54 +0900

    cern

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    oci = {
-      source = "oracle/oci"
-    }
-  }
-}
-
-provider "oci" {
-  region              = "us-sanjose-1"
-  auth                = "SecurityToken"
-  config_file_profile = "learn-terraform"
-}
-
 resource "oci_core_vcn" "internal" {
   dns_label      = "internal"
   cidr_block     = "172.16.0.0/20"
